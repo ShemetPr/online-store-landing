34bd0e9999d5dfadedc023bb413214d31b615ffe
AnastasiyaShemet <Anastasiya>
2022-08-01 17:32:10 +0300

    add: add js/ajax script

--- js/popup-form.js
+++ js/popup-form.js
@@ -1,27 +1,9 @@
 var modalWindow = document.getElementById('modal-1');
-var btn = document.getElementById("catalog-btn-open");
-var btn2 = document.getElementById("catalog-btn-open-2");
-var btn3 = document.getElementById("catalog-btn-open-3");
-var btn4 = document.getElementById("catalog-btn-open-4");
-var btn5 = document.getElementById("catalog-btn-open-5");
+
 var btn6 = document.getElementById("catalog-btn-open-6");
 var span = document.getElementsByClassName("popup-close-btn")[0];
 
-btn.onclick = function() {
-   modalWindow.style.display = "block";
- }
-btn2.onclick = function() {
-    modalWindow.style.display = "block";
-}
-btn3.onclick = function() {
-    modalWindow.style.display = "block";
-}
-btn4.onclick = function() {
-    modalWindow.style.display = "block";
-}
-btn5.onclick = function() {
-    modalWindow.style.display = "block";
-}
+
 btn6.onclick = function() {
     modalWindow.style.display = "block";
 }
